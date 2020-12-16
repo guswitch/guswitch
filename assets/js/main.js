950564f281531509e097fda794e9c2585fe7f2c7
guswitch <gustaavohenrique@hotmail.com>
2020-12-15 21:17:34 -0300

    atualizaçãode interface (beta)

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ var chartElements = document.querySelectorAll('.chart');
 chartElements.forEach((chartElement) => {
 	
     new EasyPieChart(chartElement, {
-        barColor:'#5778cc',//Pie chart colour
+        barColor:'#020616',//Pie chart colour
 		trackColor: '#e3e3e3',
 		scaleColor: false,
 		lineWidth : 7,
@@ -63,4 +63,24 @@ imagesLoaded(isotopeContainer, function () {
 
 AOS.init();
 
+
+
+const options = {
+	bottom: '32px', // default: '32px'
+	right: '32px', // default: 'unset'
+	time: '0.8s', // default: '0.3s'
+	mixColor: '#fff', // default: '#fff'
+	backgroundColor: '#fff',  // default: '#fff'
+	backgroundColorDark: '#303030',  // default: '#fff'
+	buttonColorDark: '#303030',  // default: '#100f2c'
+	buttonColorLight: '#fff', // default: '#fff'
+	saveInCookies: false, // default: true,
+	label: '🌓', // default: ''
+	autoMatchOsTheme: true // default: true
+  }
+
+function addDarkmodeWidget() {
+    new Darkmode(options).showWidget();
+  }
+  window.addEventListener('load', addDarkmodeWidget);
 

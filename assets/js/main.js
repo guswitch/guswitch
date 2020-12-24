567799ec1c2b132e41d4385f2f9b9bfb8555d583
guswitch <gustaavohenrique@hotmail.com>
2020-12-24 17:16:38 -0300

    recurso de download de CV adicionado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,6 @@
 "use strict";
 
-/* ======= Pie Chart ========= */
-/* Ref: https://github.com/rendro/easy-pie-chart */
-
-
-var chartElements = document.querySelectorAll('.chart');
-
-chartElements.forEach((chartElement) => {
-	
-    new EasyPieChart(chartElement, {
-        barColor:'#020616',//Pie chart colour
-		trackColor: '#e3e3e3',
-		scaleColor: false,
-		lineWidth : 7,
-		animate: 1000,
-    });
-    
-});
-
-
-/* ======= Isotope plugin ======= */
-/* Ref: http://isotope.metafizzy.co/ */
-// init Isotope 
+// Initiallization Isotope 
 
 const isotopeContainer = document.querySelector('.isotope');
 const filterItems = document.querySelectorAll('#filters .type');
@@ -62,25 +41,4 @@ imagesLoaded(isotopeContainer, function () {
 // Initiallization AOS
 
 AOS.init();
-
-
-
-const options = {
-	bottom: '32px', // default: '32px'
-	right: '32px', // default: 'unset'
-	time: '0.8s', // default: '0.3s'
-	mixColor: '#fff', // default: '#fff'
-	backgroundColor: '#fff',  // default: '#fff'
-	backgroundColorDark: '#303030',  // default: '#fff'
-	buttonColorDark: '#303030',  // default: '#100f2c'
-	buttonColorLight: '#fff', // default: '#fff'
-	saveInCookies: false, // default: true,
-	label: '🌓', // default: ''
-	autoMatchOsTheme: true // default: true
-  }
-
-function addDarkmodeWidget() {
-    new Darkmode(options).showWidget();
-  }
-  window.addEventListener('load', addDarkmodeWidget);
 
